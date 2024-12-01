7cbbb3592476610fa5dd200be8248211a32775ea
Matisse <93315053+MatissePrt@users.noreply.github.com>
2024-12-01 16:25:07 +0100

    update port ok 2

--- linkup-api/index.js
+++ linkup-api/index.js
@@ -9,6 +9,8 @@ import { initBlobClient } from "./config/blobStorage.js";
 
 // Initialisation de l'application
 const app = express();
+
+console.log('test port :' +  process.env.PORT)
 const PORT =  process.env.PORT || 3000
 
 // Middleware
