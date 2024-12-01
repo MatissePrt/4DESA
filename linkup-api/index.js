2d13953d98eed4078ac56d1c235f39e99a62f092
Matisse <93315053+MatissePrt@users.noreply.github.com>
2024-12-01 16:38:23 +0100

    Update index.js

--- linkup-api/index.js
+++ linkup-api/index.js
@@ -10,7 +10,7 @@ import { initBlobClient } from "./config/blobStorage.js";
 // Initialisation de l'application
 const app = express();
 
-const PORT =  process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 
 // Middleware
 app.use(express.json()); // Pour analyser les requêtes au format JSON
